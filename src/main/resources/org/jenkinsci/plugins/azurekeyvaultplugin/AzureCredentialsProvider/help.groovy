da3336dba9cf522b34c013b20f33164b6dd25373
gavinfish <drfish.me@gmail.com>
2020-01-21 16:21:02 +0800

    Fix copywirte date

--- src/main/resources/org/jenkinsci/plugins/azurekeyvaultplugin/AzureCredentialsProvider/help.groovy
+++ src/main/resources/org/jenkinsci/plugins/azurekeyvaultplugin/AzureCredentialsProvider/help.groovy
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  * <p>
- * Copyright (c) 2017 Microsoft Corporation
+ * Copyright (c) 2020 Microsoft Corporation
  * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
